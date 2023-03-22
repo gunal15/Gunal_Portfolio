af9f500fd2dcf4ebb265738c82f6132f0b594e03
gunal15 <gunalnandakumar@gmail.com>
2023-03-22 22:54:12 +0530

    resume updated

--- src/Components/Header/RTA.js
+++ src/Components/Header/RTA.js
@@ -3,7 +3,7 @@ import React from 'react';
 const RTA = () => {
   return (
     <div className='rta'>
-        <a href='https://drive.google.com/file/d/1RgDY1a3joNj_9Va3M9tDmWZDcN_qLH_m/view?usp=sharing' className='btn' target='_blank' rel="noreferrer">Get Resume</a>
+        <a href='https://drive.google.com/drive/folders/1ItqMaWmSTJqokPcindsI7coxYp9Nlj3U?usp=sharing' className='btn' target='_blank' rel="noreferrer">Get Resume</a>
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
